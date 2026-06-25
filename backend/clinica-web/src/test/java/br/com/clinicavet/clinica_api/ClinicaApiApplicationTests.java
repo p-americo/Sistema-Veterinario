bf7124e4a595c6732404cda2b7aa5d622261c8a3
Vitória Silva <silva.lopes.vitoria@gmail.com>
2026-06-24 22:42:06 -0300

    Ajustes ci/cd

--- backend/clinica-web/src/test/java/br/com/clinicavet/clinica_api/ClinicaApiApplicationTests.java
+++ backend/clinica-web/src/test/java/br/com/clinicavet/clinica_api/ClinicaApiApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.clinicavet.clinica_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class ClinicaApiApplicationTests {
 
 	@Test
